4c3db04d67484bf344d77071e417caeab48eaba5
Oleh Radchenko <or435654@gmail.com>
2023-11-07 21:55:01 +0400

    Zadania po zajęciach(zrobione zadania 1 i 4)

---
+++ Zadania(07.11.2023)/Zadanie1/script.js
@@ -0,0 +1,30 @@
+function kalk(form, char){
+    let l1 = parseFloat(form.liczba1.value);
+    let l2 = parseFloat(form.liczba2.value);
+    let er;
+    switch(char){
+        case '+':
+            wynik = l1+l2;
+            break;
+        case '-':
+            wynik = l1-l2;
+            break;
+        case '*':
+            wynik = l1*l2;
+            break;
+        case '/':
+            if(l2==0){
+                er = true;
+            }else{
+                wynik = l1/l2;
+            }
+            break;
+        default:
+            wynik = 0;
+    }
+    if(er){
+        document.getElementById("wynik").innerHTML = "Nie można dzielić przez 0 ;c";
+    }else{
+        document.getElementById("wynik").innerHTML = "Wynik twojego działania to: "+wynik;
+    }
+}
